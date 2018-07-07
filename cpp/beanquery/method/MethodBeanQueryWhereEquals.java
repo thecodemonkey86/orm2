c3e2190ae8808952de496858fad1df7526bc5ccc
stoeckerb <bernhard_stoecker@yahoo.de>
2018-07-08 00:17:06 +0200

    fix query equals helper method for nullable columns

--- cpp/beanquery/method/MethodBeanQueryWhereEquals.java
+++ cpp/beanquery/method/MethodBeanQueryWhereEquals.java
@@ -2,13 +2,16 @@ package cpp.beanquery.method;
 
 import cpp.Types;
 import cpp.bean.BeanCls;
+import cpp.bean.Nullable;
 import cpp.beanquery.ClsBeanQuery;
 import cpp.core.Method;
 import cpp.core.Param;
 import cpp.core.QString;
+import cpp.core.instruction.IfBlock;
 import cpp.lib.ClsAbstractBeanQuery;
 import cpp.lib.ClsQVariant;
 import database.column.Column;
+import cpp.core.Type;
 
 public class MethodBeanQueryWhereEquals extends Method{
 	BeanCls bean;
@@ -17,13 +20,21 @@ public class MethodBeanQueryWhereEquals extends Method{
 	public MethodBeanQueryWhereEquals(ClsBeanQuery query, BeanCls bean,Column c) {
 		super(Public, query.toRef(), "where"+c.getUc1stCamelCaseName()+"Equals");
 		this.bean=bean;
-		pValue = addParam(new Param(BeanCls.getDatabaseMapper().columnToType(c), "value"));
+		Type t = BeanCls.getDatabaseMapper().columnToType(c);
+		pValue = addParam(new Param(t.isPrimitiveType() ? t : t.toConstRef(), "value"));
 		this.c = c;
 	}
 
 	@Override
 	public void addImplementation() {
-		_return( _this().callMethod(ClsAbstractBeanQuery.where, QString.fromStringConstant("b1."+ c.getEscapedName()+"=?"), Types.QVariant.callStaticMethod(ClsQVariant.fromValue, pValue) ));
+		if(c.isNullable()) {
+			IfBlock ifNull = _if(pValue.callMethod(Nullable.isNull));
+			ifNull.thenBlock()._return( _this().callMethod(ClsAbstractBeanQuery.where, QString.fromStringConstant("b1." + c.getEscapedName()+" is null")) );
+			ifNull.elseBlock()._return( _this().callMethod(ClsAbstractBeanQuery.where, QString.fromStringConstant("b1." + c.getEscapedName()+"=?"), Types.QVariant.callStaticMethod(ClsQVariant.fromValue,  pValue.callMethod(Nullable.val) )) );
+		} else {
+			_return( _this().callMethod(ClsAbstractBeanQuery.where, QString.fromStringConstant( "b1." + c.getEscapedName()+"=?"), Types.QVariant.callStaticMethod(ClsQVariant.fromValue, pValue) ));
+		}
+		
 	}
 	@Override
 	public boolean includeIfEmpty() {
