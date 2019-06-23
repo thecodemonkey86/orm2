0bea4a3c98320b2614c9e7bc479fa26e7a20b1eb
stoeckerb <bernhard_stoecker@yahoo.de>
2019-06-23 23:10:39 +0200

    -

--- cpp/core/expression/EqualOperator.java
+++ cpp/core/expression/EqualOperator.java
@@ -1,13 +1,13 @@
 package cpp.core.expression;
 
-import cpp.Types;
+import cpp.CoreTypes;
 import cpp.core.Operator;
 import cpp.core.Param;
 
 public abstract class EqualOperator extends Operator {
 
 	public EqualOperator(Param p) {
-		super("==", Types.Bool, false);
+		super("==", CoreTypes.Bool, false);
 		if (p == null) {
 			System.out.println();
 		}
